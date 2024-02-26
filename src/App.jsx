abc6399237f400a3f703118cea424e1e358223e6
João Andrade <dev.jandrade@gmail.com>
2024-02-26 16:07:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import maincontent from './components/maincontent'
+import sidebar from './components/sidebar'
+
+import './styles/components/app.sass'
+
+function App() {
+
+  return (
+    <div id="portifo">
+     <h1>João Victor Andrade</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/maincontent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const maincontent = () => {
+  return (
+    <div>maincontent</div>
+  )
+}
+
+export default maincontent
